94cbed267ac7234f4d0425f2715aca66fb7ad6f1
mateus <lsants.dev@gmail.com>
2024-03-20 16:13:36 -0300

    Ajustar componentes na pagina home

--- src/app/pages/home/home-page/home-page.component.ts
+++ src/app/pages/home/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from '../../../shared/side-bar/side-bar.component';
 import { FeedComponent } from '../components/feed/feed.component';
+import { TitleBarComponent } from '../../../shared/title-bar/title-bar.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [SideBarComponent, FeedComponent],
+  imports: [SideBarComponent, FeedComponent, TitleBarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
